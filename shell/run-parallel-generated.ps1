81984a2f3d4048d196d651eefe11a79971e178d5
senthilsb-toast <senthil.sb@toasttab.com>
2023-09-12 20:20:30 +0530

    added tests-generated folder

--- shell/run-parallel-generated.ps1
+++ shell/run-parallel-generated.ps1
@@ -2,5 +2,3 @@
 
 $testargs = '-file .\shell\testrun.ps1 -testfile "./testcasefiles/sample-tests.xlsx" -sheet "15" -baseurl "https://letcode.in/windows" -userid "bbbb" -password "ccccs" -report "report" -email "email"'
 Start-Process powershell.exe -ArgumentList $testargs
-$testargs = '-file .\shell\testrun.ps1 -testfile "./testcasefiles/sample-tests.xlsx" -sheet "2" -baseurl "https://the-internet.herokuapp.com/" -userid "abc" -password "abcd" -report "" -email ""'
-Start-Process powershell.exe -ArgumentList $testargs
