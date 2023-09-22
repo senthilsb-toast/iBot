9944e4d6bf7ee4efd4a17a9bc53daa30c1e1a985
senthilsb-toast <senthil.sb@toasttab.com>
2023-09-22 13:09:13 +0530

    updated sample generated files

--- shell/run-parallel-generated.sh
+++ shell/run-parallel-generated.sh
@@ -1,3 +1,4 @@
 #Start-Process powershell.exe -WindowStyle Hidden -ArgumentList $args
 echo "run-parallel-generated.sh....................................."
-./shell/testrun.sh "./testcasefiles/sample-tests.xlsx" "12" "" "bbbb" "ccccs" "report" 
+./shell/testrun.sh "./testcasefiles/sample-tests.xlsx" "15" "https://letcode.in/windows/" "bbbb" "ccccs" "report" ""report""
+./shell/testrun.sh "./testcasefiles/sample-tests.xlsx" "2" "https://the-internet.herokuapp.com/" "abc" "abcd" "report" 
